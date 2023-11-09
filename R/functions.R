1b2fc33c5398cf86df3d880df6fe3a3c502e54a3
Ulises-H-Guzman <uliseshguzman@gmail.com>
2023-11-09 10:31:15 +0100

    tragtes function

--- R/functions.R
+++ R/functions.R
@@ -1,2 +1,17 @@
 usethis::use_git(".Rbuildignore")
-1
+
+# Code chunk
+
+#' Basic statistics
+#'
+#' @param data lipidomics
+#'
+#' @return A data.frame/tibble
+
+descriptive_stats <- function(data) {
+    data %>%
+        dplyr::group_by(metabolite) %>%
+        dplyr::summarise(across(value, list(mean = mean, sd = sd))) %>%
+        dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ round(.x, digits = 1)))
+
+}
