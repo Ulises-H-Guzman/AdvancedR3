e8b0ff00cd18c4673358a493091f0ce861190ae2
Ulises-H-Guzman <uliseshguzman@gmail.com>
2023-11-09 14:47:46 +0100

    .Rbuildignore

--- R/functions.R
+++ R/functions.R
@@ -13,8 +13,7 @@ descriptive_stats <- function(data) {
         dplyr::group_by(metabolite) %>%
         dplyr::summarise(across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ round(.x, digits = 1)))
-
-}
+    }
 
 
 #' Title
@@ -28,4 +27,33 @@ plot_distributions <- function(data){
         facet_wrap(vars(metabolite),scales = "free")
 }
 
+
+
+
+#' Column values string to snake_case
+#'
+#' @param data with string columns
+#' @param cols the column to convert into snakecase
+#'
+#' @return a data frame
+
+column_values_to_snake_case <- function(data, cols) {
+    data %>%
+        dplyr::mutate(dplyr::across({{ cols }}, snakecase::to_snake_case))
+}
+
+
+
+
+#' Pivot to wider
+#'
+#' @param data lipidomics
+#'
+#' @return wider data_frame
+
+metabolites_to_wider = function(data){
+    data %>% tidyr::pivot_wider(names_from = metabolite, value_from = value,
+                                values_fn = mean,
+                                names_prefix = "metabolite_")
+}
 
